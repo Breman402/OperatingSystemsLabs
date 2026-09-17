8c60171c7f0d035b428cef5a2418c9e98c46dd66
Måns Bremer <mansbre@student.chalmers.se>
2026-09-17 15:52:38 +0200

    Co-authored-by: malinalcen <malinalcen@users.noreply.github.com> Co-authored-by: Felix Baur <felix.baur@uni-ulm.de>

--- lab_1/code/lsh.c
+++ lab_1/code/lsh.c
@@ -21,8 +21,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <signal.h>
 
 // The <unistd.h> header is your gateway to the OS's process management facilities.
 #include <unistd.h>
@@ -30,15 +32,25 @@
 #include "parse.h"
 
 static void print_cmd(Command *cmd);
+void runCMD(Command *cmd);
 static void print_pgm(Pgm *p);
 void stripwhite(char *);
 
 int main(void)
 {
+  signal(SIGINT, SIG_IGN); // Ignore signal SIGINT (Ctrl+C) in the parent process
   for (;;)
   {
     char *line;
     line = readline("> ");
+
+    // This fixes the ctrl + D func.
+    if (line == NULL) {
+    printf("\n");
+    break;
+  } else if (line && strcmp(line, "exit") == 0) { // Check if the input line is "exit"
+    break;
+  }
 
     // Remove leading and trailing whitespace from the line
     stripwhite(line);
@@ -53,10 +65,9 @@ int main(void)
       {
         // Print the parsed command
         print_cmd(&cmd);
-
+        
         // Create a new process for the parsed command and run it?
         runCMD(&cmd);
-
       }
       else
       {
@@ -118,48 +129,124 @@ static void print_pgm(Pgm *p)
   }
 }
 
-// Start a new operating system process to run a command.
-void runCMD(Command *cmd) {
-  pid_t pid = fork();
-  
-  // 
-  
-  if (pid < 0) {
-    // Fork failed
-    perror("Fork failed");
-    exit(EXIT_FAILURE);
 
+
+// Start a new operating system process to run a command.
+void run_simple(Command *cmd) {
+  int background = cmd->background;
+  pid_t pid = fork();
+
+  if (pid < 0) {
+    fprintf(stderr, "Fork failed");
   } else if (pid == 0) {
     // Child process
-    
-    // use execvp to run the command
-    // command to execute:
 
+    if (background == 0) { // if not a background process, die from ctrl + c
+      // This setting only applies to this child process! 
+      signal(SIGINT, SIG_DFL);
+    }
+      
     // Look at the programlist, fetch the top one:
     const char* commandToExecute = cmd->pgm->pgmlist[0];
     
     // Look at the programlist, fetch the rest of the arguments:
     char *const *argument = cmd->pgm->pgmlist;
     
+    // Execute the command with the provided arguments
     execvp(commandToExecute, argument);
-
+    
   } else {
     // Parent process
-    int background = cmd->background;
     
-    if (background == 0) {
-      // Wait for the child process to finish
+    if (background == 0) { // if not a background process
+      // Wait for the child process to finish or a ctrl + c signal to be recived
       waitpid(pid, NULL, 0);
+
     } else if (background == 1) {
       // Do not wait for the child process to finish
+      waitpid(pid, NULL, WNOHANG); // Non-blocking wait to reap the child process if it has already finished
+      
       printf("Process running in background with PID: %d\n", pid);
     } else {
       // Invalid background value
       fprintf(stderr, "Invalid background value: %d\n", background);
     }
   }
-
 }
+
+
+void run_piped(Command *cmd) {
+  int background = cmd->background;
+  Pgm *last  = cmd->pgm;       // last command in the pipeline
+  Pgm *first = cmd->pgm->next; // first command in the pipeline
+
+  int fd[2]; //communication channels
+  if (pipe(fd) == -1) { perror("pipe"); return; }
+
+  //child 1 (writes in pipe)
+  pid_t pid1 = fork();
+  
+  if (pid1 < 0) {
+    fprintf(stderr, "Fork failed");
+  }
+  else if (pid1 == 0) {
+    if (background == 0) {
+      signal(SIGINT, SIG_DFL);
+    }
+    dup2(fd[1], 1);
+    close(fd[0]); close(fd[1]);
+    execvp(first->pgmlist[0], first->pgmlist);
+    exit(1);
+  
+  }
+
+  //child 2 (reading from pipe)
+  pid_t pid2 = fork();
+  if (pid2 < 0) {
+    fprintf(stderr, "Fork failed");
+  }
+  else if (pid2 == 0) {
+    if (background == 0) signal(SIGINT, SIG_DFL);
+    dup2(fd[0], 0);
+    close(fd[0]); close(fd[1]);
+    execvp(last->pgmlist[0], last->pgmlist);
+    exit(1);
+  }
+
+
+    // parent close the pipe
+    close(fd[0]);
+    close(fd[1]);
+
+    
+    
+    if (background == 0) {
+      // Wait for the child process to finish or a ctrl + c signal to be recived
+      waitpid(pid1, NULL, 0);
+      waitpid(pid2, NULL, 0);
+      
+    } else if (background == 1) {
+      // Do not wait for the child process to finish
+      printf("Process running in background with PID: %d\n", pid1);
+    } else {
+      // Invalid background value
+      fprintf(stderr, "Invalid background value: %d\n", background);
+    }
+  
+}
+
+void runCMD(Command *cmd_list) {
+  if (cmd_list->pgm->next == NULL){
+    run_simple(cmd_list);
+  }else{
+    run_piped(cmd_list);
+  }
+}
+
+
+
+
+
 
 /* Strip whitespace from the start and end of a string.
  *
